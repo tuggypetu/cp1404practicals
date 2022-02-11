268011cbed33302ab21541464a86f0ec46eaae90
tuggypetu <tuggypetu@gmail.com>
2022-02-11 21:52:17 +0800

    Refactor password check program to use functions

--- prac3/password_check.py
+++ prac3/password_check.py
@@ -1,8 +1,27 @@
 """Get password and print asteriks of password length"""
 
+"""Constants"""
 MIN_PASS_LENGTH = 5
-get_password = input("Enter password: ")
-while len(get_password) < 5:
-    print(f"Invalid. Minimum password length is {MIN_PASS_LENGTH}.")
-    get_password = input("Enter password: ")
-print('*' * len(get_password))
+
+
+def main():
+    """Main function"""
+    geta_password = get_pwd()
+    asteriks = get_asteriks(geta_password)
+    print(asteriks)
+
+
+def get_pwd():
+    geta_password = input("Enter password: ")
+    while len(geta_password) < 5:
+        print(f"Invalid. Minimum password length is {MIN_PASS_LENGTH}.")
+        geta_password = input("Enter password: ")
+    return geta_password
+
+
+def get_asteriks(geta_password):
+    asteriks = '*' * len(geta_password)
+    return asteriks
+
+
+main()
