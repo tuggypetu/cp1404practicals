641d5bcc02eb84eb86b4d6a74085fcf0f7390f80
tuggypetu <tuggypetu@gmail.com>
2022-02-12 16:38:13 +0800

    update code and add username checker

--- prac4/subject_reader.py
+++ prac4/subject_reader.py
@@ -7,12 +7,18 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Main function"""
     data = get_data()
     print(data)
+    print("----------")
+    inline_data(data)
+    print("\n----------")
+    username_check()
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    full_list = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -21,9 +27,28 @@ def get_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        full_list.append(parts)
         print(parts)  # See if that worked
         print("----------")
     input_file.close()
+    return full_list
+
+
+def inline_data(data):
+    """Prints the data in proper sentences"""
+    for i in data:
+        print(f"{i[0]} is taught by {i[1]:12} and has {i[2]:3} students")
+
+
+def username_check():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Enter username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 main()
