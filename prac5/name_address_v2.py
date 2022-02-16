47b22872356e15094471ec2cc06858d22a5e6d48
tuggypetu <tuggypetu@gmail.com>
2022-02-16 19:33:33 +0800

    add update to add file load and save

---
+++ prac5/name_address_v2.py
@@ -0,0 +1,57 @@
+"""Menu for name and addresses with file loading and saving"""
+
+
+def main():
+    # address_dict = {"Jordan": "Blk 18 Jalan Sultan 01-150", "Michael": "19 Hougang Avenue 3 01-185",
+    #                 "Idiot": "141 MIDDLE ROAD, #06-05", "Retard": "501 ORCHARD ROAD, #04-09",
+    #                 "Wangdu": "1 Sophia Road 03-23", "Kai": "5 Eng Kong Terrace"}
+    # file = open("name_address_v2.txt", 'w')
+    # for name in address_dict:
+    #     file.write(f"{name}: {address_dict[name]}\n")
+    # file.close()
+    address_dict = {}
+    file = open("name_address_v2.txt", 'r')
+    for line in file:
+        line = line.strip()
+        name, address = line.split(': ')
+        address_dict[name] = address
+    file.close()
+    print(f"Current dictionary is: {address_dict}")
+    menu = "(E)nter a new name & address" \
+           "\n(C)hange an address for an existing entry" \
+           "\n(P)rint the address for a name you choose" \
+           "\nChoose E, C, or P"
+    print(menu)
+    choice = input(">>> ").upper()
+    while choice != "":
+        if choice == "E":
+            name = input("Enter name: ").title()
+            address = input("Enter address: ").title()
+            address_dict[name] = address
+        elif choice == "C":
+            name = input("Enter name: ").title()
+            if name in address_dict:
+                address = input(f"Enter new address for {name}: ").title()
+                address_dict[name] = address
+            else:
+                print("Invalid. Name does not exist in dictionary.")
+        elif choice == "P":
+            name = input("Enter name: ").title()
+            if name in address_dict:
+                print(f"{name} lives in {address_dict[name]}.\n")
+            else:
+                print("Invalid. Name does not exist in dictionary.")
+        else:
+            print("Invalid choice")
+        print(f"Current dictionary is: {address_dict}")
+        print(menu)
+        choice = input(">>> ").upper()
+    print(f"\nThis is the dictionary at the end: {address_dict}")
+    print("\nProgram has quit.")
+    file = open("name_address_v2.txt", 'w')
+    for name in address_dict:
+        file.write(f"{name}: {address_dict[name]}\n")
+    file.close()
+
+
+main()
