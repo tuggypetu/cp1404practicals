3cbf7624152e2b0c192bde66c2f6ca3eebecdfec
tuggypetu <tuggypetu@gmail.com>
2022-02-19 14:07:44 +0800

    add Guitar class and all guitar program files

--- prac6/guitar.py
+++ prac6/guitar.py
@@ -23,3 +23,7 @@ class Guitar:
     def is_vintage(self):
         """Return T is more than 50 years old(vintage)"""
         return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Less than, used for sorting Guitars - by year released."""
+        return self.year < other.year
