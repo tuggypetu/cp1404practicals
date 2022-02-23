202f19194e22c6427a7320195cbeed940513562e
tuggypetu <tuggypetu@gmail.com>
2022-02-23 22:16:16 +0800

    update squaring.kv/.py

--- prac7/squaring.py
+++ prac7/squaring.py
@@ -9,14 +9,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Siddhanth Biswas'
 
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (300, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
