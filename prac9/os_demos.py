df8c8c0e9b7c7af653771d2822d7b1f2844e8469
tuggypetu <tuggypetu@gmail.com>
2022-03-08 16:27:38 +0800

    add os demo file

--- prac9/os_demos.py
+++ prac9/os_demos.py
@@ -18,9 +18,11 @@ def main():
 
     # Make a new directory
     # The next time you run this, it will crash if the directory exists
-    # TODO: Use exception handling to avoid the crash (just pass)
-    os.mkdir('temp')
-
+    # TODOne: Use exception handling to avoid the crash (just pass)
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
         # Ignore directories, just process files
@@ -30,12 +32,12 @@ def main():
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
 
-        # TODO: Try these options one at a time
+        # TODOne: Try these options one at a time
         # Option 1: rename file to new name - in place
         # os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -55,6 +57,27 @@ def demo_walk():
 
         # TODO: add a loop to rename the files
 
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            # print(full_name)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            # print(new_name)
+            os.rename(full_name, new_name)
 
-main()
-# demo_walk()
+        # My creative way (also dumb and slow)-
+
+        # if directory_name == '.':
+        #     for i in subdirectories:
+        #         os.chdir('/Users/siddhanth/Documents/Download_Stuff/CP1404/cp1404practicals/prac9/Lyrics/{}'.format(i))
+        #         for filename in os.listdir('.'):
+        #             # Ignore directories, just process files
+        #             if os.path.isdir(filename):
+        #                 continue
+        #             new_name = get_fixed_filename(filename)
+        #             os.rename(filename, new_name)
+        # os.chdir('/Users/siddhanth/Documents/Download_Stuff/CP1404/cp1404practicals/prac9/Lyrics')
+        # print("\tand renamed files:", filenames)
+
+
+# main()
+demo_walk()
