380bbfe59738c6c95c9655eb6d398ff5d40db07f
tuggypetu <tuggypetu@gmail.com>
2022-03-08 20:02:53 +0800

    update minor update

--- prac9/os_demos.py
+++ prac9/os_demos.py
@@ -37,7 +37,7 @@ def main():
         # os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        shutil.move(filename, 'temp/' + new_name)
+        # shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
