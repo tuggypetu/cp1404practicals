2430ffe81cb1fbecdb4134f2026d255f62bd9a2a
tuggypetu <tuggypetu@gmail.com>
2022-03-08 18:31:40 +0800

    add cleanup file

---
+++ prac9/cleanup_files.py
@@ -0,0 +1,91 @@
+"""Cleanup files"""
+
+
+import shutil
+import os
+
+
+def main():
+    """Demo os module functions."""
+    print("Starting directory is: {}".format(os.getcwd()))
+
+    # Change to desired directory
+    os.chdir('Lyrics/Sunday')
+
+    # Print a list of all files in current directory
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+
+    # Make a new directory
+    # The next time you run this, it will crash if the directory exists
+    # TODOne: Use exception handling to avoid the crash (just pass)
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        print("Renaming {} to {}".format(filename, new_name))
+
+        # TODOne: Try these options one at a time
+        # Option 1: rename file to new name - in place
+        # os.rename(filename, new_name)
+
+        # Option 2: move file to new place, with new name
+        shutil.move(filename, 'temp/' + new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    letter_list = [letter for letter in new_name]
+    new_name = ''
+    for i, letter in enumerate(letter_list):
+        new_l = letter
+        if i != 0 and i != (len(letter_list) - 1):
+            if letter in ['(', ')']:
+                if letter == '(':
+                    if letter_list[i - 1] != '_':
+                        new_l = '_' + letter
+                elif letter == ')':
+                    if letter_list[i+1] not in ['_', '.']:
+                        new_l = letter + '_'
+            if letter.islower():
+                if not letter_list[i - 1].isalpha() and letter_list[i - 1] != '.':
+                    new_l = letter.upper()
+                elif letter_list[i + 1].isupper():
+                    new_l = letter + '_'
+            elif letter.isupper() and letter_list[i+1].isupper():
+                new_l = letter + '_'
+        elif i == 0:
+            new_l = letter.upper()
+
+        new_name += new_l
+    return new_name
+
+
+def demo_walk():
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        # TODOne: add a loop to rename the files
+
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            # print(full_name)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            # print(new_name)
+            os.rename(full_name, new_name)
+
+
+main()
+# demo_walk()
