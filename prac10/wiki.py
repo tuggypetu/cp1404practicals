47483e9b59a45b3a9a7bee04e0ab7e0a9236f614
tuggypetu <tuggypetu@gmail.com>
2022-03-11 22:50:04 +0800

    update wiki

--- prac10/wiki.py
+++ prac10/wiki.py
@@ -17,11 +17,20 @@ def main():
             print(page.url)
             print(page.summary)
 
-        except wikipedia.exceptions.DisambiguationError as error:
-            print(f"\nThe options for title '{title.title()}' are:\n", error.options, '\n')
-            new_page_name = wikipedia.page(error.options[0], auto_suggest=False)
+        except wikipedia.exceptions.DisambiguationError:
+
+            page_titles = wikipedia.search(title)
+            if page_titles[0] != title.title():
+                title_new = page_titles[0]
+            elif page_titles[1].lower() == page_titles[0].lower():
+                title_new = page_titles[2]
+            else:
+                title_new = page_titles[1]
+            # print(title_new)
+            print(f"\nThe options for title '{title.title()}' are:\n", page_titles, '\n')
+            new_page_name = wikipedia.page(title_new, auto_suggest=False)
             print(f"Page title is assumed to be '{new_page_name.title}'. It could possibly need a more specific title.")
-            print(new_page_name.url, '\n', wikipedia.summary(error.options[0]))
+            print(new_page_name.url, '\n', wikipedia.summary(title_new))
         except wikipedia.exceptions.PageError:
             print("No such Wikipedia page.")
 
